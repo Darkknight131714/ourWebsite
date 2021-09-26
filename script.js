2da73f468d13f21540722655a80ec87bd17f9527
Darkknight131714 <darkknight131714@gmail.com>
2021-09-27 01:59:41 +0530

    Start

---
+++ script.js
@@ -0,0 +1,16 @@
+// function changeCss () {
+//     var bodyElement = document.querySelector("body");
+//     var name;
+//     if(this.scrollY>250){
+//         name="download.png";
+//         bodyElement.style.backgroundPosition="top left";
+//         bodyElement.style.backgroundColor="#E2F1FF";
+//     }
+//     else{
+//         name="bg.png";
+//         bodyElement.style.backgroundPosition="top right";
+//         bodyElement.style.backgroundColor="white";
+//     }
+//     bodyElement.style.backgroundImage="url("+name+")";
+//   }
+//   window.addEventListener("scroll", changeCss , false);
